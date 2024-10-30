584f2dce49ea24f2f1524ae617f5c03fd74181a4
Administrator <z965412166@163.com>
2024-10-30 14:59:57 +0800

    完善火球技能

---
+++ Source/GDProject/Public/AbilitySystem/Abilities/FireBoltAbility.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GDProjectileSpellAbility.h"
+#include "FireBoltAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GDPROJECT_API UFireBoltAbility : public UGDProjectileSpellAbility
+{
+	GENERATED_BODY()
+};
